15b75fde9e343d3caf32e9bbf3d4b91d4471d0e2
Donatas <adamonis@mail.com>
2020-09-17 14:51:41 +0300

    refactored quality check booleans

--- src/gildedRose.js
+++ src/gildedRose.js
@@ -16,24 +16,27 @@ class Shop {
       const isAgedBrie = item.name === 'Aged Brie';
       const isBackstagePass = item.name === 'Backstage passes to a TAFKAL80ETC concert';
       const isSulfuras = item.name === 'Sulfuras, Hand of Ragnaros';
+      // Booleans to check quality
+      const isQualityPositive = item.quality > 0;
+      const isQualityLessThan50 = item.quality < 50;
 
       if (!isAgedBrie && !isBackstagePass) {
-        if (item.quality > 0) {
+        if (isQualityPositive) {
           if (!isSulfuras) {
             item.quality = item.quality - 1;
           }
         }
       } else {
-        if (item.quality < 50) {
+        if (isQualityLessThan50) {
           item.quality = item.quality + 1;
           if (isBackstagePass) {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
+              if (isQualityLessThan50) {
                 item.quality = item.quality + 1;
               }
             }
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
+              if (isQualityLessThan50) {
                 item.quality = item.quality + 1;
               }
             }
@@ -46,7 +49,7 @@ class Shop {
       if (item.sellIn < 0) {
         if (!isAgedBrie) {
           if (!isBackstagePass) {
-            if (item.quality > 0) {
+            if (isQualityPositive) {
               if (!isSulfuras) {
                 item.quality = item.quality - 1;
               }
@@ -55,7 +58,7 @@ class Shop {
             item.quality = item.quality - item.quality;
           }
         } else {
-          if (item.quality < 50) {
+          if (isQualityLessThan50) {
             item.quality = item.quality + 1;
           }
         }
